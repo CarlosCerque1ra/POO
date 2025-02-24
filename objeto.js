ea573b9245fe1cbe48e88b18f5cce71a49a90406
CarlosCerque1ra <carlos.a.senai02@gmail.com>
2025-02-24 15:54:48 -0300

    Initial commit

---
+++ objeto.js
@@ -0,0 +1,12 @@
+// Criando um objeto de Aluno
+
+const aluno = {
+    nome: "Carlos",
+    idade: 16,
+    
+    estudar: () => console.log
+    (`${aluno.nome} esta estudando.`)
+};
+
+console.log (aluno.nome);
+aluno.estudar();
